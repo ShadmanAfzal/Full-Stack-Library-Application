b432431afb6139bb3ea25a4c325068e0d9561b2b
Shadman12345 <shadman.afzal@appyhigh.com>
2022-11-13 18:24:00 +0530

    Added Hashrouter as default router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { AddBook } from './components/Books/AddBook';
 import { EditBook } from './components/Books/EditBook';
 import { SearchBooks } from './components/Books/SearchBooks';
@@ -8,12 +8,12 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/add' element={<AddBook />} />
       <Route path='/edit/:id' exact element={<EditBook />} />
       <Route path='/search' element={<SearchBooks />} />
     </Routes>
-  </Router>
+  </HashRouter>
 );
